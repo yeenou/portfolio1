feaab05e58a863fa6b439dfa4e8cfa897bff5d3e
yeenou <cye0328@naver.com>
2022-02-07 06:15:53 +0900

    location trans

--- js/location.js
+++ js/location.js
@@ -23,24 +23,24 @@ var markerOptions =[
     {
         title:"본점", 
         latlng : new kakao.maps.LatLng(37.50711796614849,126.7564159502457),
-        imgSrc : 'img/marker1.png', 
-        imgSize : new kakao.maps.Size(232,99), 
+        imgSrc : 'img/maps.png', 
+        imgSize : new kakao.maps.Size(99,99), 
         imgPos : {offset: new kakao.maps.Point(116, 99)},
         button : branch_btns[0]
     },
     {
         title:"지점1", 
         latlng : new kakao.maps.LatLng(33.450701, 126.570667),
-        imgSrc : 'img/marker2.png', 
-        imgSize : new kakao.maps.Size(232,99), 
+        imgSrc : 'img/maps.png', 
+        imgSize : new kakao.maps.Size(99,99), 
         imgPos : {offset: new kakao.maps.Point(116, 99)},
         button : branch_btns[1]
     },
     {
         title:"지점2", 
         latlng : new kakao.maps.LatLng(37.557527,126.9222836),
-        imgSrc : 'img/marker3.png', 
-        imgSize : new kakao.maps.Size(232,99), 
+        imgSrc : 'img/maps.png', 
+        imgSize : new kakao.maps.Size(99,99), 
         imgPos : {offset: new kakao.maps.Point(116, 99)}, 
         button : branch_btns[2]
     }
